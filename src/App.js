46e1e478de571ab6833c208a216a4ed129678c41
Yihan <sherry1570304111@gmail.com>
2025-07-11 10:38:54 -0700

    connect to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch(`https://express-sample-q591.onrender.com/api/hello`)
+      .then(res => res.json())
+      .then(data => {
+        console.log('Data fetched:', data);
+        setMsg(data.msg)
+      }).catch(err => {
+        console.error('Error fetching data:', err);
+        setMsg('Error fetching data');
+      })
+      .finally(() => {setIsLoading(false);
+        console.log(msg);
+      });
+  }, []);
+
   return (
     <div>
       <h1>My Custom React App</h1>
